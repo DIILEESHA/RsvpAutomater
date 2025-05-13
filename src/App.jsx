bfa8ce6f4fb78b1ea5d2086fff7e17c5bddbaac9
DIILEESHA <dileeshanawarathna18@gmail.com>
2025-05-13 20:33:02 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {
   Routes,
   Route,
   useLocation,
+  Navigate,
 } from "react-router-dom";
 import { Layout } from "antd";
 import AdminDashboard from "./components/AdminDashboard";
@@ -13,29 +14,44 @@ import RSVPForm from "./components/RSVPForm";
 import Login from "./components/Login";
 import "./App.css";
 import ThankYouPage from "./components/ThankYouPage";
+import { auth } from "./firebase";
 
 const { Header, Content } = Layout;
+
+// Protected Route Component
+const ProtectedRoute = ({ children }) => {
+  if (!auth.currentUser) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+};
 
 function AppLayout() {
   const location = useLocation();
 
   // Check if the current route matches /rsvp/:guestId
-  const isRSVPPage = location.pathname.startsWith("/");
+  const isRSVPPage = location.pathname.startsWith("/rsvp");
 
   return (
     <Layout className="layout">
       {!isRSVPPage && (
         <Header>
-          <h3 style={{background:"#000"}} className="logo">Wedding RSVP</h3>
+          {/* <h3 style={{background:""}} className="logo">Wedding RSVP</h3> */}
         </Header>
       )}
       <Content style={{ padding: "0 0px" }}>
         <div className="site-layout-content">
           <Routes>
             <Route path="/" element={<Login />} />
-            {/* <Route path="/dashboard" element={<AdminDashboard />} /> */}
-            <Route path="/dashboard" element={<GuestManager />} />
-            <Route path="/rsvp/:guestId" element={<RSVPForm />} />{" "}
+            <Route 
+              path="/dashboard" 
+              element={
+                <ProtectedRoute>
+                  <GuestManager />
+                </ProtectedRoute>
+              } 
+            />
+            <Route path="/rsvp/:guestId" element={<RSVPForm />} />
             <Route path="/thank-you" element={<ThankYouPage />} />
           </Routes>
         </div>
